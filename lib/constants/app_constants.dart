29f5f8baf32f3cd2df538e2025f9cc7514ad7111
paradax005 <borhen.fatnassi@esprit.tn>
2023-05-22 18:07:58 +0200

    starter project

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kDark = Color(0xFF000000);
+const kLight = Color(0xFFFFFFFF);
+const kLightGrey = Color(0x95D1CECE);
+const kDarkGrey = Color(0xFF9B9B9B);
+const kOrange = Color(0xfff55631);
+const kLightBlue = Color(0xff3663e3);
+const kDarkBlue = Color(0xff1c153e);
+const kLightPurple = Color(0xff6352c5);
+const kDarkPurple = Color(0xff6352c5);
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+
+import 'constants/app_constants.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        useInheritedMediaQuery: true,
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Job Finder',
+            theme: ThemeData(
+              scaffoldBackgroundColor: Color(kLight.value),
+              iconTheme: IconThemeData(color: Color(kDark.value)),
+              primarySwatch: Colors.grey,
+            ),
+            home: const OnBoardingScreen(),
+          );
+        });
+  }
+}
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
